793af19b0246e2711d2bc5ccc3b9e5f8709137cc
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2021-08-10 20:29:50 +0700

    Create traceback02.py

---
+++ PetrophysicsWithPython/traceback02.py
@@ -0,0 +1,6 @@
+def traceback02():
+  # Specify file path
+  filepath = 'https://raw.githubusercontent.com/yohanesnuwara/formation-evaluation/main/data/volve/15_9-F-11A.LAS'
+
+  # Read with lasio
+  las = lasio.read(filepath) 
