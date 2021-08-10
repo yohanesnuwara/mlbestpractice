a2471e1eb87e5ae49199e71f2dd468db32f81ea1
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2021-08-10 23:20:03 +0700

    Delete traceback03.py

--- PetrophysicsWithPython/traceback03.py
+++
@@ -1,5 +0,0 @@
-# Specify file path
-filepath = 'https://raw.githubusercontent.com/yohanesnuwara/formation-evaluation/main/data/volve/15_9-F-11A.LAS'
-
-# Read with lasio
-las = lasio.read(filepath)
